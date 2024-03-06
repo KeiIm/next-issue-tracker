42c9094dbe769954966cf75a156178e5251f80c8
Kei <marios_bestfriend@htmail.com>
2024-03-06 15:43:09 -0500

    Refactor: organize import statements

---
+++ app/components/index.ts
@@ -0,0 +1,9 @@
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+export { Skeleton };
+export { SkeletonTheme };
+export { default as Link } from './Link'
+export { default as ErrorMessage } from './ErrorMessage'
+export { default as IssueStatusBadge } from './IssueStatusBadge'
+export { default as Spinner } from './Spinner'
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
+import Link from './components/Link';
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return <Link href={'/issues'}>View Issues</Link>;
 }
