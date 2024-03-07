37dcf963897533d43a168b358272079cc4c058f6
Kei <marios_bestfriend@htmail.com>
2024-03-07 16:08:21 -0500

    Setup NextAuth (Auth.js)

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,3 +1,7 @@
 import IssueFormSkeleton from '../_components/IssueFormSkeleton';
 
 export default IssueFormSkeleton;
+
+// We can reorganize the URL structure in order to prevent issues-skeleton from appearing before this skeleton.
+// Not worth the trouble of restructuring. Maybe find another way to have this override if it bothers us.
+// As per video d7b94493-cc48-47ee-ab90-302110a4419d
