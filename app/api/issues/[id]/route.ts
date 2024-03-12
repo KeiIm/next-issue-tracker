7be1c2eedfe9c718ca93dca98b31e499465c4b04
Kei <marios_bestfriend@htmail.com>
2024-03-12 11:47:44 -0400

    Enhance API to assign issues (Reinstalled prisma and modules)

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,35 +1,43 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/prisma/client";
-import { IssueSchema } from "@/app/validationSchemas";
-import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
+import { patchIssueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 import delay from "delay";
 
 export async function PATCH(
     request: NextRequest,
-    { params }: { params: {id: string } }) {
-        const session = await getServerSession(authOptions)
-        if (!session)
-            return NextResponse.json({}, { status: 401 })
+    { params }: { params: {id: string } }
+) {
+    const session = await getServerSession(authOptions)
+    if (!session)
+        return NextResponse.json({}, { status: 401 })
         
     const body = await request.json()
-    const validation = IssueSchema.safeParse(body)
-
+    const validation = patchIssueSchema.safeParse(body)
     if (!validation.success)
         return NextResponse.json(validation.error.format(), { status: 400 });
 
-        const issue = await prisma.issue.findUnique({
-            where: { id: parseInt(params.id) }
-        })
+    const { assignedToUserId, title, description } = body;
 
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({ where: { id: assignedToUserId } })
+        if (!user)
+            return NextResponse.json({ error: 'Invalid user' }, { status: 400 })
+    }
+
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    })
     if (!issue)
         return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
 
     const updatedIssue = await prisma.issue.update({
         where: { id: issue.id },
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedToUserId
         }
     })
 
