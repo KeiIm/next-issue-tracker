67cefc7133c4d36c2c1ca7ed5aebfa04f9d8726f
Kei <marios_bestfriend@htmail.com>
2024-03-13 16:54:27 -0400

    Refactor: Extract IssueTable

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -29,7 +29,7 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
   };
 
   return (
-    <Flex align="center" gap="2" mt="3">
+    <Flex align="center" gap="2">
       <Button
         color="gray"
         disabled={currentPage === 1}
