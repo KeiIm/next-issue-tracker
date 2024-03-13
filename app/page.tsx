11149b3cf4c9e7ecda002502cc20e1dac80f87ca
Kei <marios_bestfriend@htmail.com>
2024-03-13 17:49:54 -0400

    Build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+import IssueSummary from './IssueSummary';
 import LatestIssue from './LatestIssue';
 import Link from './components/Link';
-import { Text } from '@radix-ui/themes';
+import { Flex, Text } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma?.issue.count({ where: { status: 'OPEN' } });
+  const inProgress = await prisma?.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma?.issue.count({ where: { status: 'CLOSED' } });
+
   return (
-    <>
-      {/* <Text>Latest Issues</Text> */}
+    <Flex direction="column">
       <LatestIssue />
-    </>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+    </Flex>
   );
 }
