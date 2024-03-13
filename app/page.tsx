9a9e362690e646bc9072bfbeb44b471ddd4c1e82
Kei <marios_bestfriend@htmail.com>
2024-03-13 16:29:35 -0400

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Link from './components/Link';
 import Pagination from './components/Pagination';
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   // return <Link href={'/issues'}>View Issues</Link>;
-  return <Pagination itemCount={100} pageSize={10} currentPage={5} />;
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 }
