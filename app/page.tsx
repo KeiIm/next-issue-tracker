65b6afa983110d40e103ad842305a686457fdf8e
Kei <marios_bestfriend@htmail.com>
2024-03-13 15:38:56 -0400

    Built pagination component layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Link from './components/Link';
+import Pagination from './components/Pagination';
 
 export default function Home() {
-  return <Link href={'/issues'}>View Issues</Link>;
+  // return <Link href={'/issues'}>View Issues</Link>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={5} />;
 }
